1c695c09027439594946738d63c3c10d2e40b00e
Stanislav Minko <minkostas@gmail.com>
2023-03-21 12:22:40 +0300

    feat: login variables

---
+++ script.js
@@ -0,0 +1,4 @@
+let inputLoginEmail = document.querySelector('.input-login-email');
+let inputLoginText = document.querySelector('.input-login-text');
+let btn = document.querySelector('button');
+let input1 = document.querySelector('.input-login-email');
