a4f1282f804455c6f36281f4aaee5c9097458439
Stanislav Minko <minkostas@gmail.com>
2023-03-22 12:24:47 +0300

    feat: login addeventListener

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let inputLoginEmail = document.querySelector('.input-login-email');
 let inputLoginText = document.querySelector('.input-login-text');
 let btn = document.querySelector('button');
 let input1 = document.querySelector('.input-login-email');
+
+console.log(input1);
+console.log(input1.value);
+
+btn.addEventListener('click',function () {
+    try {
+        if(!input1.value){
+            throw new Error('Пустое поле')
+        } else {
+            checkInput(input1);
+        }
+    } catch (error) {
+        alert(error.message);
+    }
+})
